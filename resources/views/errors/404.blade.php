e634d8016be5d6c52789ad3cce84bb97779a2fef
marvin <aguilar.marvin07@gmail.com>
2018-08-09 18:18:41 -0500

    adminlte

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,3 @@
+<h1>Error 404, estas tratando de entrar a una ruta que no existe
+  <a href="{{route('home')}}">Volver el inicio</a>
+</h1>
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+// Authentication Routes...
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
